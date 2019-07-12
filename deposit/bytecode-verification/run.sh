6b5976fbf7f988ef08010c1fe8d100829edbab41
Daejun Park <daejunpark@gmail.com>
2019-07-12 16:59:37 -0500

    update run.sh

--- deposit/bytecode-verification/run.sh
+++ deposit/bytecode-verification/run.sh
@@ -1,4 +1,7 @@
+  export OPTS="--z3-impl-timeout 500 --deterministic-functions --no-exc-wrap --cache-func-optimized --no-alpha-renaming --format-failures --boundary-cells k,pc --log-cells k,output,statusCode,localMem,pc,gas,wordStack,callData,accounts,memoryUsed,#pc,#result"
+  export OPTS+=" --log --log-rules --debug-z3-queries"
+
   make clean
   make split-proof-tests
 
-  kprove -v --debug -d ~/evm-semantics/.build/java -m VERIFICATION --z3-impl-timeout 500 --deterministic-functions --no-exc-wrap --cache-func-optimized --no-alpha-renaming --format-failures --boundary-cells k,pc --log-cells k,output,statusCode,localMem,pc,gas,wordStack,callData,accounts,memoryUsed,#pc,#result --smt-prelude ~/verified-smart-contracts/deposit/bytecode-verification/evm.smt2  ~/verified-smart-contracts/specs/deposit/getHashTreeRoot-spec.k
+  kprove -v --debug -d ~/evm-semantics/.build/java -m VERIFICATION $OPTS --smt-prelude ~/verified-smart-contracts/deposit/bytecode-verification/evm.smt2 ~/verified-smart-contracts/specs/deposit/getHashTreeRoot-spec.k
