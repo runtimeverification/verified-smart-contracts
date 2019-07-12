43aa830e56bfd536c770ec08bab4ad5c5cd41ec5
Daejun Park <daejunpark@gmail.com>
2019-07-12 16:50:06 -0500

    fix run.sh

--- deposit/bytecode-verification/run.sh
+++ deposit/bytecode-verification/run.sh
@@ -1,4 +1,4 @@
   make clean
   make split-proof-tests
 
-  kprove -v --debug -d ~/evm-semantics/.build/java -m VERIFICATION --z3-impl-timeout 500 --deterministic-functions --no-exc-wrap --cache-func-optimized --no-alpha-renaming --format-failures --boundary-cells k,pc --log-cells k,output,statusCode,localMem,pc,gas,wordStack,callData,accounts,memoryUsed,#pc,#result --smt-prelude /Users/daejunpark/ethereum/verified-smart-contracts/deposit/bytecode-verification/evm.smt2  /Users/daejunpark/ethereum/verified-smart-contracts/specs/deposit/getHashTreeRoot-spec.k
+  kprove -v --debug -d ~/evm-semantics/.build/java -m VERIFICATION --z3-impl-timeout 500 --deterministic-functions --no-exc-wrap --cache-func-optimized --no-alpha-renaming --format-failures --boundary-cells k,pc --log-cells k,output,statusCode,localMem,pc,gas,wordStack,callData,accounts,memoryUsed,#pc,#result --smt-prelude ~/verified-smart-contracts/deposit/bytecode-verification/evm.smt2  ~/verified-smart-contracts/specs/deposit/getHashTreeRoot-spec.k
