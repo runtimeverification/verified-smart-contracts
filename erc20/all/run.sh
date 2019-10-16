005d38073417eb4f513240cdefdf818ce4f9e1ac
Daejun Park <daejunpark@gmail.com>
2019-10-16 04:35:19 -0500

    erc20 verifier

---
+++ erc20/all/run.sh
@@ -0,0 +1,13 @@
+  [[ $# -eq 1 ]] || { echo "Illegal number of parameters"; exit; }
+  rm -f erc20-spec.ini
+  cat root.ini \
+    totalSupply.ini \
+    balanceOf.ini \
+    allowance.ini \
+    approve.ini \
+    transfer.ini \
+    transferFrom.ini \
+    $1 >erc20-spec.ini
+  make clean
+  make
+  make -i -j 8 test
