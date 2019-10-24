471a8c4e90bed2ee38582664d0e9daa2e0c7c03f
Denis Bogdanas <denis.bogdanas@gmail.com>
2019-10-24 17:46:40 +0300

    erc20/all: added top 15 mainnet erc20 contracts

---
+++ erc20/all/run-mainnet.sh
@@ -0,0 +1,11 @@
+  bash -x run.sh mainnet-specs/01_tether.ini
+  bash -x run.sh mainnet-specs/02_bnb.ini
+  bash -x run.sh mainnet-specs/04_chainlink.ini
+  bash -x run.sh mainnet-specs/05_huobi.ini
+  bash -x run.sh mainnet-specs/06_maker.ini
+  bash -x run.sh mainnet-specs/08_crypto.ini
+  bash -x run.sh mainnet-specs/09_ino.ini
+  bash -x run.sh mainnet-specs/10_bat.ini
+  bash -x run.sh mainnet-specs/11_insight.ini
+  bash -x run.sh mainnet-specs/13_hedg.ini
+  bash -x run.sh mainnet-specs/14_zrx.ini
